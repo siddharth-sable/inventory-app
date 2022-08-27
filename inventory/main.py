6bbd0400d5098247f9c4b2bb1dc6656fc6c5b072
Siddharth Sabale <siddharth.sable4618@gmail.com>
2022-08-28 02:43:13 +0530

    Update main.py

--- inventory/main.py
+++ inventory/main.py
@@ -12,9 +12,9 @@ app.add_middleware(
 )
 
 redis = get_redis_connection(
-    host="redis-11419.c305.ap-south-1-1.ec2.cloud.redislabs.com",
-    port="11419",
-    password="moScabx7MrAIYCAxeoN64qJbysLGzmb8",
+    host="redis-labs-url-here",
+    port="port-here",
+    password="password-here",
     decode_responses=True
 
 )
